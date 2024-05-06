67050335ee5deefbe87b884f168ef7d2a7283efe
Hazem Ya-sin <hazem.yasin.1416@gmail.com>
2024-05-06 07:40:27 +0300

    done here

--- sandBox1/angular-app1/src/app/app.component.ts
+++ sandBox1/angular-app1/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-app1';
+  title = 'title of the first angular app but not the last one';
 }
