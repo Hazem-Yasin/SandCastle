f07b6532b3e3916d97681533f9e816fc76c8fb15
Hazem Ya-sin <hazem.yasin.1416@gmail.com>
2024-06-09 10:41:15 +0300

    added some stuff I have no idea about

--- 17/MyApiProject/MyApiProject/Models/Item.cs
+++ 17/MyApiProject/MyApiProject/Models/Item.cs
@@ -1,10 +1,18 @@
 ﻿namespace MyApiProject.Models
 {
+    // Define a model class to represent an item in the application
     public class Item
     {
+        // Property to hold the unique identifier for the item
+        // 'Id' is an integer that serves as the primary key for the item
         public int Id { get; set; }
+
+        // Property to hold the name of the item
+        // 'Name' is a string representing the name of the item
         public string Name { get; set; }
+
+        // Property to hold the value of the item
+        // 'Value' is a string representing the value associated with the item
         public string Value { get; set; }
     }
-
 }
